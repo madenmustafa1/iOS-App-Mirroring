b27c148d6abb00228e3043de91417014d20a5309
Mustafa Maden <67011777+madenmustafa1@users.noreply.github.com>
2024-04-07 00:33:19 +0300

    Initial Commit

---
+++ iOS-App-Mirroring/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-App-Mirroring
+//
+//  Created by Mustafa Maden on 7.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
